385bee6323e8e1d2f5d8aed6efa994d5d5700ecb
gabu <gabujr337@gmail.com>
2025-07-16 11:58:48 +0300

    c arrays with ASCII concept

--- arrays.c
+++ arrays.c
@@ -1,30 +1,30 @@
-/* count digits, white spaces, others */
 #include <stdio.h>
 
-int main() {
+/* count digits, white space, others */
+int main()
+  {
   int c, i, nwhite, nother;
   int ndigit[10];
-
   nwhite = nother = 0;
   for (i = 0; i < 10; ++i) {
     ndigit[i] = 0;
+    printf("%d", ndigit);
   }
 
   while ((c = getchar()) != EOF) {
-    if (c >= '0' && c <= '9') {
-      ++ndigit[c - '0'];
-      }
-    else if ((c == ' ') || (c == '\n') || (c == '\t')) {
+    if (c >= '0' && c <= '9')
+      ++ndigit[c-'0'];
+    else if (c == ' ' || c == '\n' || c == '\t')
       ++nwhite;
-    }
-    else {
+    else
       ++nother;
-    }
-    printf("digits =");
-    for (i = 0; i < 10; ++i) {
-      printf(" %d", ndigit[i]);
-    }
-    printf(", white space = %d, other = %d\n",
-        nwhite, nother);
   }
-}
+
+  printf("digits =");
+  for (i = 0; i < 10; ++i) {
+    printf(" %d", ndigit[i]);
+  }
+
+  printf(", white space = %d, other = %d\n",
+  nwhite, nother);
+  }
