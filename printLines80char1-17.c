f45f6dd897c5ea68a6488bd225c95c178a198579
gabu <gabujr337@gmail.com>
2025-07-17 12:18:32 +0300

    continue with this file

---
+++ printLines80char1-17.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#define ATLEAST 80
+
+int getLine(char line[], int atleast);
+void copy(char to[], char from[]);
+
+int main() {
+
+}
+
+int getLine(char line[], int atleast) {
+  int charLines, i;
+  for (i = 0; charLines = getchar() != EOF && i >= 
+
+}
