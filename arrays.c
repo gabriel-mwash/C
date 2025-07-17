05e16bdd977a5b7e5508518d704a38fc360c02a6
gabu <gabujr337@gmail.com>
2025-07-17 10:19:07 +0300

    more on arrays

--- arrays.c
+++ arrays.c
@@ -39,20 +39,13 @@ int main() {
   int age[5];
 
   printf("enter 5 input valuws: ");
+  for (int i = 0; i < 5; ++i) {
+    scanf("%d", &age[i]);
+  }
 
-  scanf("%d", &age[0]);
-  scanf("%d", &age[1]);
-  scanf("%d", &age[2]);
-  scanf("%d", &age[3]);
-  scanf("%d", &age[4]);
-
-
-  printf("%d ", age[0]);
-  printf("%d ", age[1]);
-  printf("%d ", age[2]);
-  printf("%d ", age[3]);
-  printf("%d ", age[4]);
-
+  for (int i = 0; i < 5; ++i) {
+    printf("%d ", age[i]);
+  }
 };
 
 
