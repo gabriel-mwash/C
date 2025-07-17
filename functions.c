076b5db52190e80e4f2ef94a9db8c3509e7f66ca
gabu <gabujr337@gmail.com>
2025-07-17 10:19:59 +0300

    more on functions, by val

--- functions.c
+++ functions.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-int power(int m, int n);
+int power(int m, int n); // func prototype
+/* or int power( int, int) */
 
 /* test power function */ 
 int main() {
   int i;
-  for (i = 1; i < 10; ++i) {
+  for (i = 0; i < 10; ++i) {
     printf("%d %d %d\n", i, power(2, i), power(-3, i));
-    return 0;
   }
+  return 0;
 }
 
-/* power: raise base to nth power; n >= 0 */
-int power(int base, int n) {
+/* power : raise base to the n-th power; n >= 0 */
+int power (int base, int n) {
   int i, p;
   p = 1;
   for (i = 1; i <= n; ++i) {
@@ -20,3 +21,5 @@ int power(int base, int n) {
   }
   return p;
 }
+
+
